317d949f50855f9b8353fdc3a639f8d780d4e1da
root <chefa@list.ru>
2021-07-09 14:28:33 +0300

    add 0.5 routers

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
 //import logo from './logo.svg';
 import './App.css';
-import Header from './components/Header';
-import SideBar from './components/SideBar';
-import MyPage from './components/MyPage';
+import Header from './components/Header/Header';
+import Message from './components/Message/Message';
+import Music from './components/Music/Music';
+import News from './components/News/News';
+import Profile from './components/Profile/Profile';
+import SideBar from './components/SideBar/SideBar';
+import Settings from './components/Settings/Settings';
+
 
 const App = () => {
   return (
+
     <div className="main-div">
       <Header />
       <SideBar />
-      <MyPage />
+      <div>
+        <Profile />
+        <Message />
+        <News />
+        <Music />
+        <Settings />
+      </div>
     </div>
+
   );
-}
+};
 
 export default App;
