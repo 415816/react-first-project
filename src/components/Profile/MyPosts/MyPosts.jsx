51139466182b3aed98428107f9cf3fd476a680ae
Vit <415816@bk.ru>
2021-07-11 04:43:14 +0300

    messages, dialogs and posts transition to new array with help map

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -2,15 +2,19 @@ import mp from './MyPosts.module.css';
 import NewPost from './NewPost/NewPost';
 import Post from './Post/Post';
 
-const MyPosts = () => {
-return(<div className={mp.my_posts}><h3>My posts</h3>
-      <NewPost />
-      <Post message='Hi, how are you?' likes='5'/>
-      <Post message='It is my first post!' likes='3'/>
-      <Post message='It is my zero post!' likes='18'/>
+let arrPosts = [
+  { likes: 5, textPost: 'Hi, how are you?' },
+  { likes: 3, textPost: 'It is my first post!' },
+  { likes: 18, textPost: 'It is my zero post!' },
+];
 
-      
-    </div>)
+let posts = arrPosts.map(pos => <Post message={pos.textPost} likes={pos.likes} />);
+
+const MyPosts = () => {
+  return (<div className={mp.my_posts}><h3>My posts</h3>
+    <NewPost />
+    {posts}
+  </div>)
 }
 
 export default MyPosts;
