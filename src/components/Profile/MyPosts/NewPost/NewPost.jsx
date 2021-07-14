eaeb5eebd5120e7606fde91a787a90b3755068f7
Vit <415816@bk.ru>
2021-07-15 02:02:29 +0300

    add ref

--- src/components/Profile/MyPosts/NewPost/NewPost.jsx
+++ src/components/Profile/MyPosts/NewPost/NewPost.jsx
@@ -1,9 +1,20 @@
+import React, { createRef } from 'react';
 import nP from './NewPost.module.css';
 
+
+
 const NewPost = () => {
+   let newPostText = React.createRef();
+
+   let addNewPost = () =>{
+      let text = newPostText.current.value;
+      alert(text);
+   }
+   
+
 return(<div className={nP.newPost}>
-        <input placeholder="Plese input your news"></input>
-        <div><button>Send</button></div>
+        <input ref={newPostText}  placeholder="Plese input your news"></input>
+        <div><button onClick={addNewPost}>Send</button></div>
       </div>
    )
 }
