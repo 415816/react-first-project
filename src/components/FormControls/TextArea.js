e0c2c996503d268e12e5d5687bbfd14ed9dec653
Vit <415816@bk.ru>
2021-09-11 21:51:00 +0300

    add visual signals about errors for Messages

--- src/components/FormControls/TextArea.js
+++ src/components/FormControls/TextArea.js
@@ -1,11 +1,11 @@
-import sta from
+import sta from './textArea.module.css'
 
 export const TextArea = ({input, meta, ...props}) => {
     const hasError = meta.touched && meta.error;
     return (
         <div className={sta.textAreaWrapper}>
             <textarea className={hasError ? sta.borderRed : ""} {...input} {...props}/>
-            {hasError && <span>'nsffjsndfjsndfjns'</span>}
+            {hasError && <span>{meta.error}</span>}
         </div>
     )
 }
